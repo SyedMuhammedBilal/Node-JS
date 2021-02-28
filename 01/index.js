881ef42c11f87fe121c9e64e53387d580fa7f5aa
Syed Muhammed Bilal <intergo878@gmail.com>
2021-02-28 11:19:56 +0500

    class 01

---
+++ 01/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Server listening');
+});
+
+const port = 8080
+
+server.listen(port, () => {
+    console.log('server started')
+});
