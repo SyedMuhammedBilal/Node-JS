6f59357f63f501b1f291f9f69fc5130f10f174a9
Syed Muhammed Bilal <intergo878@gmail.com>
2021-03-03 12:35:16 +0500

    class 05

---
+++ 05/index.js
@@ -0,0 +1,19 @@
+/*
+    Events Module 
+    NodeJS has a built-in function called "Events"
+    where u can create, fire, and listen your own events
+
+    Example 01: for registering, the event will fire only once
+    Example 02: create and event emitter instance and register a couple of callbacks
+    Example 03: Registering for the event with callback parameters
+*/
+
+const EventEmitter = require('events');
+
+const event = new EventEmitter();
+
+event.on("Printer", () => {
+    console.log('Hello World')
+});
+
+event.emit('Printer');
