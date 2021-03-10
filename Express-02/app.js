2ded093756b7e152783114f006a4c1bdc18e2e1d
Syed Muhammed Bilal <intergo878@gmail.com>
2021-03-10 14:02:46 +0500

    JSON data

--- Express-02/app.js
+++ Express-02/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+// ExpressJS will stringify the object to JSON data in BTS
 app.get('/', (req, res) => {
-    res.send('hello world :)');
+    res.send([
+        {
+            id: 1,
+            name: 'Syed Bilal',
+            age: 18
+        }
+    ]);
 });
 
 app.listen(8080, () => {
