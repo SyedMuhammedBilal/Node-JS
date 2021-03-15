190eaa15a53aae24211102dc4c4448d2c59d985c
Syed Muhammed Bilal <intergo878@gmail.com>
2021-03-15 21:24:53 +0500

    new mongoose server

---
+++ Express-03-Mongo/src/app.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// creating a connection b/w DataBase and NodeJS
+const uri = "mongodb+srv://syed_bilal:1234@cluster0.wwm7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('connection successful')
+}).catch(err => {
+    console.log(err)
+});
