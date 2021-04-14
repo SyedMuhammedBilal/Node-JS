cd1cf46eb6cac4d527767c649c1b5427dfda0b06
Syed Muhammed Bilal <intergo878@gmail.com>
2021-04-14 13:32:17 +0500

    new commit

--- REST-API-02/src/app.js
+++ REST-API-02/src/app.js
@@ -4,11 +4,21 @@ require('./db/connection')
 const PlayersRanking = require('./models/index')
 const app = express();
 
+app.use(express.json());
+
 app.get('/', async (req, res) => {
     res.send('hello from homepage')
 });
 
-app.post()
+app.post('/player', async (req, res) => {
+    try {
+        const addingPlayer = new PlayersRanking(req.body);
+        const insertPlayer = await addingPlayer.save();
+        res.send(insertPlayer);
+    } catch (error) {
+        res.send(error);
+    };
+});
 
 app.listen(port, () => {
     console.log('server started')
