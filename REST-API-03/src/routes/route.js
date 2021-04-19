7f8b4816e713a7fb55311d1e27d58c63ffc6dde6
Syed Muhammed Bilal <intergo878@gmail.com>
2021-04-19 18:17:03 +0500

    new commit

--- REST-API-03/src/routes/route.js
+++ REST-API-03/src/routes/route.js
@@ -12,28 +12,66 @@ router.get('/', (req, res) => {
     res.send('home page')
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { name, email, phone, work, password, cPassword } = req.body;
 
     if(!name || !email || !phone || !work || !password || !cPassword) {
-        res.status(400).json({error: 'Please enter all fields'})
+        res.status(422).json({error: 'Please enter all fields'})
     }
 
-    User.findOne({ email: email })
-        .then((userExist) => {
-            if(userExist) {
-                return res.status(422).json({error: 'user already exist'});
-            }
+    // User.findOne({ email: email })
+    //     .then((userExist) => {
+    //         if(userExist) {
+    //             return res.status(422).json({error: 'user already exist'});
+    //         }
 
-            const user = new User({ name, email, phone, work, password, cPassword });
-            user.save()
-                .then(() => {
-                    res.status(201).json({ message: 'user registered successfully' })
-                }).catch((error) => {
-                    console.log(error)
-                    res.status(500).json({ error })
-                })
-        })
+    //         const user = new User({ name, email, phone, work, password, cPassword });
+    //         user.save()
+    //             .then(() => {
+    //                 res.status(201).json({ message: 'user registered successfully' })
+    //             }).catch((error) => {
+    //                 console.log(error)
+    //                 res.status(500).json({ error })
+    //             })
+    //     })
+
+    try {
+        const response = await User.findOne({ email: email });
+        if(response) {
+            return res.status(422).json({ error: 'user already exist' })
+        }
+        
+        const user = new User({ name, email, phone, work, password, cPassword });
+
+        await user.save();
+
+        res.status(201).json({ message: 'user registered successfully' });
+        
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ error })
+    }
 })
+
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if(!email || !password) {
+            res.status(400).json({message: "please fill out all fields"})
+        }
+
+        const userLogin = await User.findOne({ email });
+
+        if(!userLogin) {
+            res.status(400).json({ error: "invalid credentials" })
+        } else {
+            res.status(201).json({ message: "user logged in successfully" })
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error)
+    };
+});
 
 module.exports = router
