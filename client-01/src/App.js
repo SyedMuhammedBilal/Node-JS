9c2bb00caae5edc8587c9c46eb8491e504d5b9e6
Syed Muhammed Bilal <intergo878@gmail.com>
2021-04-25 12:51:37 +0500

    new commit

--- client-01/src/App.js
+++ client-01/src/App.js
@@ -5,6 +5,7 @@ import About from './components/About'
 import Login from './components/Signin'
 import Signup from './components/Signup'
 import Contact from './components/Contact'
+import Error from './components/404Error'
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
@@ -12,11 +13,12 @@ function App() {
     <div>
       <Navbar />
       <Switch>
-      <Route exact path="/home" component={Home} /> 
-<Route exact path="/about" component={About} /> 
-<Route exact path="/login" component={Login} /> 
-<Route exact path="/signup" component={Signup} /> 
-<Route exact path="/contact" component={Contact} /> 
+        <Route exact path="/home" component={Home} /> 
+        <Route exact path="/about" component={About} /> 
+        <Route exact path="/login" component={Login} /> 
+        <Route exact path="/signup" component={Signup} /> 
+        <Route exact path="/contact" component={Contact} /> 
+        <Route exact path="/*" component={Error} /> 
       </Switch>
     </div>
   );
---
+++ client-01/src/components/404Error.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <center>
+                <h1>404 Error</h1>
+            </center>
+        </div>
+    )
+}
+
+export default Error
