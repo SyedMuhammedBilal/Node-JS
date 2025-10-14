fd21dd598b4557c99c77091bc25b9019c3a5bc2a
Syed Muhammed Bilal <intergo878@gmail.com>
2025-10-15 02:02:20 +0500

    Sync from Codfy - 2025-10-14T21:02:20.164Z

--- REST-API-01/src/routes/Students.js
+++ REST-API-01/src/routes/Students.js
@@ -5,65 +5,56 @@ const Student = require('../Schema/studentSchema');
 // Create a new student
 router.post('/students', async (req, res) => {
     try {
-        const student = new Student(req.body);
-        const createStudent = await student.save();
-        res.status(201).send(createStudent);
-    } catch (error) {
-        res.status(400).send(error);
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    } catch (e) {
+        res.status(400).send(e);
     }
 });
 
-// Get all students
+// Read all students
 router.get('/students', async (req, res) => {
     try {
-        const students = await Student.find();
-        res.status(200).send(students);
-    } catch (error) {
-        res.status(500).send(error);
+        const studentsData = await Student.find();
+        res.status(200).send(studentsData);
+    } catch (e) {
+        res.status(400).send(e);
     }
 });
 
-// Get a specific student by ID
+// Read individual student by ID
 router.get('/students/:id', async (req, res) => {
     try {
         const _id = req.params.id;
-        const student = await Student.findById(_id);
-        if (!student) {
+        const studentData = await Student.findById(_id);
+
+        if (!studentData) {
             return res.status(404).send();
+        } else {
+            res.status(200).send(studentData);
         }
-        res.status(200).send(student);
-    } catch (error) {
-        res.status(500).send(error);
+    } catch (e) {
+        res.status(500).send(e);
     }
 });
 
-// Update a student by ID
+// Update student by ID
 router.patch('/students/:id', async (req, res) => {
     try {
         const _id = req.params.id;
-        const updatedStudent = await Student.findByIdAndUpdate(_id, req.body, {
-            new: true // Returns the updated document
+        const updateStudents = await Student.findByIdAndUpdate(_id, req.body, {
+            new: true, // Return the updated document
+            runValidators: true // Run schema validators on update
         });
-        if (!updatedStudent) {
-            return res.status(404).send();
-        }
-        res.status(200).send(updatedStudent);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
 
-// Delete a student by ID
-router.delete('/students/:id', async (req, res) => {
-    try {
-        const _id = req.params.id;
-        const deletedStudent = await Student.findByIdAndDelete(_id);
-        if (!deletedStudent) {
+        if (!updateStudents) {
             return res.status(404).send();
         }
-        res.status(200).send(deletedStudent);
-    } catch (error) {
-        res.status(500).send(error);
+
+        res.status(200).send(updateStudents);
+    } catch (e) {
+        res.status(400).send(e);
     }
 });
 
